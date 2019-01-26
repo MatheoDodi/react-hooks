f60cc5d8f1e6ac05c28e5ac70afb54b7a5f3fada
Matthew Dodi <matthew.dodi@gmail.com>
2019-01-26 13:30:37 -0800

    type: feat - Added new useEffect method to fetch initial data from the Firebase database

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,16 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const todo = props => {
   const [todoName, setTodoName] = useState('');
   const [todos, setTodos] = useState([]);
 
+  useEffect(() => {
+    axios
+      .get('https://react-hooks-matthew.firebaseio.com/todos.json')
+      .then(res => {
+        const ids = res.data;
+        const fetchedTodos = [];
+        for (const key in ids) {
+          fetchedTodos.push(ids[key].name);
+        }
+
+        setTodos(fetchedTodos);
+      });
+
+    return () => {
+      console.log('cleanup!');
+    };
+  }, []);
+
   const inputChangeHandler = e => {
     setTodoName(e.target.value);
   };
 
-  const addTodoHandler = todo => {
-    setTodos([...todos, todo]);
+  const addTodoHandler = () => {
+    setTodos([...todos, todoName]);
     setTodoName('');
+    axios
+      .post('https://react-hooks-matthew.firebaseio.com/todos.json', {
+        name: todoName
+      })
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
   };
 
   return (
@@ -21,12 +46,12 @@ const todo = props => {
         value={todoName}
         onChange={inputChangeHandler}
       />
-      <button type='button' onClick={() => addTodoHandler(todoName)}>
+      <button type='button' onClick={addTodoHandler}>
         Add
       </button>
       <ul>
         {todos.map(todo => (
-          <li>{todo}</li>
+          <li key={todo + Date.now().toString()}>{todo}</li>
         ))}
       </ul>
     </React.Fragment>
