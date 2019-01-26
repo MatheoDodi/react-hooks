150ac57e773eb149562bf217d85acc29a3cc9536
Matthew Dodi <matthew.dodi@gmail.com>
2019-01-26 00:51:10 -0800

    type: refactored inputChangeHandler

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,13 +3,17 @@ import React, { Component, useState } from 'react';
 const todo = props => {
   const [todo, setTodo] = useState('');
 
+  const inputChangeHandler = e => {
+    setTodo(e.target.value);
+  };
+
   return (
     <React.Fragment>
       <input
         type='text'
         placeholder='Todo'
         value={todo}
-        onChange={e => setTodo(e.value)}
+        onChange={inputChangeHandler}
       />
       <button type='button'>Add</button>
       <ul />
