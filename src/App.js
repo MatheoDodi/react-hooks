5de49c1ae955972707a5cf3c4a2a517dd390052d
Matthew Dodi <matthew.dodi@gmail.com>
2019-01-27 13:46:11 -0800

    type: feat - Created new Auth.js component and auth-context.js component that handle a simple authentication - Passed authState in to the other components using the Context API

--- src/App.js
+++ src/App.js
@@ -3,22 +3,29 @@ import React, { useState } from 'react';
 import Todo from './components/Todo';
 import Header from './components/Header';
 import Auth from './components/Auth';
+import AuthContext from './auth-context';
 
 const App = () => {
   const [page, setPage] = useState(<Todo />);
+  const [authStatus, setAuthStatus] = useState(false);
 
-  const loadTodosHandler = () => {
-    setPage(<Todo />);
+  const switchPage = pageName => {
+    setPage(pageName);
   };
 
-  const loadAuthHandler = () => {
-    setPage(<Auth />);
+  const changeAuthStatus = () => {
+    setAuthStatus(!authStatus);
   };
 
   return (
     <div className='App'>
-      <Header onLoadTodos={loadTodosHandler} onLoadAuth={loadAuthHandler} />
-      {page}
+      <AuthContext.Provider value={{ status: authStatus, changeAuthStatus }}>
+        <Header
+          onLoadTodos={() => switchPage(<Todo />)}
+          onLoadAuth={() => switchPage(<Auth />)}
+        />
+        {page}
+      </AuthContext.Provider>
     </div>
   );
 };
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import AuthContext from '../auth-context';
 
-const Auth = props => <h1>Auth Component</h1>;
+const Auth = props => (
+  <AuthContext.Consumer>
+    {({ status, changeAuthStatus }) => {
+      return (
+        <div>
+          <h1>{status ? 'Logged In' : 'Logged Out'}</h1>
+          <button onClick={changeAuthStatus}>Change Status</button>
+        </div>
+      );
+    }}
+  </AuthContext.Consumer>
+);
 
 export default Auth;
